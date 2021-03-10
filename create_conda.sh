540be91f60279cb81da3ef0d001c7abffa6df88b
Taylor Salo <tsalo006@fiu.edu>
2021-03-10 11:18:59 -0500

    Add environment stuff.

---
+++ create_conda.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+module add miniconda3-4.5.11-gcc-8.2.0-oqs2mbg
+conda create -p conda_env/ pip python=3.7
+source activate conda_env/
+conda install pip numpy scipy libgfortran sympy ply funcsigs cython matplotlib seaborn pandas numexpr scikit-learn tornado accelerate Biopython dateutil
+/home/data/nbc/misc-projects/Salo_PowerReplication/code/conda_env/bin/pip install pip -U
+/home/data/nbc/misc-projects/Salo_PowerReplication/code/conda_env/bin/pip install ipython jupyterlab ipywidgets nibabel nilearn
+/home/data/nbc/misc-projects/Salo_PowerReplication/code/conda_env/bin/pip install -e /home/data/nbc/misc-projects/Salo_PowerReplication/code/
+conda list > python_requirements.txt
