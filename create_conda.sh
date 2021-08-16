2a502f08392a7b2d5b08fb791e44f6490a59a39a
Taylor Salo <tsalo006@fiu.edu>
2021-08-16 12:24:31 -0400

    Successful environment build.

--- create_conda.sh
+++ create_conda.sh
@@ -22,15 +22,16 @@ conda install \
 
 /home/data/nbc/misc-projects/Salo_PowerReplication/code/conda_env/bin/pip install \
     accelerate \
+    duecredit \
     ipython \
     ipywidgets \
     jupyterlab \
     nibabel \
     nilearn \
-    rapidtide==2.0.5 \
+    git+https://github.com/tsalo/rapidtide.git@f86237d8867222d645dd53556cc4617ba4d7ebde \
     git+https://github.com/ME-ICA/tedana.git@3147e01bfbd2a058f41af73eb55debb3dea519b2 \
     git+https://github.com/ME-ICA/godec.git@094129688775e0c07ad81e946974fa0ef72f34c4 \
-    git+https://github.com/physiopy/peakdet.git@f6908e3cebf2fdc31ba73f2b3d3370bf7dfae89c \
+    git+https://github.com/tsalo/peakdet.git@0034b2c76669a227295501e0d7c7d1fd207259d1 \
     git+https://github.com/tsalo/phys2denoise.git@be8251db24b157c9a7717f3e2e41eca60ed23649
 
 conda list > python_requirements.txt
