38bf066719619417589e3ae3c455d9267d7c2bfe
Taylor Salo <tsalo006@fiu.edu>
2022-01-10 15:24:14 -0500

    Experiment 1, analysis group 5 functions.

--- experiment-1/05_resp_and_fc.py
+++ experiment-1/05_resp_and_fc.py
@@ -21,18 +21,145 @@ with each of the following used as the quality measure:
 - mean RV
 - mean RVT
 """
+import os
+import os.path as op
+import sys
+
+import numpy as np
+import pandas as pd
+
+sys.path.append("..")
+
+from utils import get_prefixes_mni, get_target_files, run_reduced_analyses  # noqa: E402
 
 
 def run_ddmra_of_rpv(participants_file, target_file_patterns):
     """Run QC:RSFC and high-low analyses on derivatives against RPV."""
-    ...
+    OUT_DIR = (
+        "/home/data/nbc/misc-projects/Salo_PowerReplication/analyses/"
+        "experiment01_group05_analysis01"
+    )
+
+    participants_df = pd.read_table(participants_file)
+    n_subs_all = participants_df.shape[0]
+    # Limit to participants with RPV value
+    participants_df = participants_df.dropna(subset="rpv")
+    print(f"{participants_df.shape[0]}/{n_subs_all} participants retained.")
+    prefix = get_prefixes_mni()["dset-dupre"]
+
+    rpv_confounds = participants_df["rpv"].values
+
+    for filetype, pattern in target_file_patterns.items():
+        filetype_out_dir = op.join(OUT_DIR, filetype.replace(" ", "_"))
+        os.makedirs(filetype_out_dir, exist_ok=True)
+        target_files = []
+        for i, row in participants_df.iterrows():
+            participant_id = row["participant_id"]
+            filename = pattern.format(participant_id=participant_id, prefix=prefix)
+            target_files.append(filename)
+
+        run_reduced_analyses(target_files, rpv_confounds, out_dir=filetype_out_dir)
 
 
 def run_ddmra_of_mean_rv(participants_file, confounds_pattern, target_file_patterns):
     """Run QC:RSFC and high-low analyses on derivatives against mean RV."""
-    ...
+    OUT_DIR = (
+        "/home/data/nbc/misc-projects/Salo_PowerReplication/analyses/"
+        "experiment01_group05_analysis02"
+    )
+
+    participants_df = pd.read_table(participants_file)
+    n_subs_all = participants_df.shape[0]
+    # Limit to participants with RPV value
+    participants_df = participants_df.dropna(subset="rpv")
+    print(f"{participants_df.shape[0]}/{n_subs_all} participants retained.")
+    prefix = get_prefixes_mni()["dset-dupre"]
+
+    rv_confounds = []
+    for i, row in participants_df.iterrows():
+        participant_id = row["participant_id"]
+        confounds_file = confounds_pattern.format(participant_id=participant_id)
+        confounds_df = pd.read_table(confounds_file)
+
+        mean_rv = np.mean(confounds_df["RVRegression_RV"].values)
+        rv_confounds.append(mean_rv)
+
+    for filetype, pattern in target_file_patterns.items():
+        filetype_out_dir = op.join(OUT_DIR, filetype.replace(" ", "_"))
+        os.makedirs(filetype_out_dir, exist_ok=True)
+        target_files = []
+        for i, row in participants_df.iterrows():
+            participant_id = row["participant_id"]
+            filename = pattern.format(participant_id=participant_id, prefix=prefix)
+            target_files.append(filename)
+
+        run_reduced_analyses(target_files, rv_confounds, out_dir=filetype_out_dir)
 
 
 def run_ddmra_of_mean_rvt(participants_file, confounds_pattern, target_file_patterns):
     """Run QC:RSFC and high-low analyses on derivatives against mean RVT."""
-    ...
+    OUT_DIR = (
+        "/home/data/nbc/misc-projects/Salo_PowerReplication/analyses/"
+        "experiment01_group05_analysis03"
+    )
+
+    participants_df = pd.read_table(participants_file)
+    n_subs_all = participants_df.shape[0]
+    # Limit to participants with RPV value
+    participants_df = participants_df.dropna(subset="rpv")
+    print(f"{participants_df.shape[0]}/{n_subs_all} participants retained.")
+    prefix = get_prefixes_mni()["dset-dupre"]
+
+    rvt_confounds = []
+    for i, row in participants_df.iterrows():
+        participant_id = row["participant_id"]
+        confounds_file = confounds_pattern.format(participant_id=participant_id)
+        confounds_df = pd.read_table(confounds_file)
+
+        mean_rvt = np.mean(confounds_df["RVTRegression_RVT"].values)
+        rvt_confounds.append(mean_rvt)
+
+    for filetype, pattern in target_file_patterns.items():
+        filetype_out_dir = op.join(OUT_DIR, filetype.replace(" ", "_"))
+        os.makedirs(filetype_out_dir, exist_ok=True)
+        target_files = []
+        for i, row in participants_df.iterrows():
+            participant_id = row["participant_id"]
+            filename = pattern.format(participant_id=participant_id, prefix=prefix)
+            target_files.append(filename)
+
+        run_reduced_analyses(target_files, rvt_confounds, out_dir=filetype_out_dir)
+
+
+if __name__ == "__main__":
+    print("Experiment 1, Analysis Group 5")
+    in_dir = "/home/data/nbc/misc-projects/Salo_PowerReplication/dset-dupre/"
+    participants_file = op.join(in_dir, "participants.tsv")
+    confounds_pattern = op.join(
+        in_dir,
+        "derivatives/power/{participant_id}/func",
+        "{participant_id}_task-rest_run-1_desc-confounds_timeseries.tsv",
+    )
+    TARGET_FILE_PATTERNS = get_target_files()
+    TARGETS = [
+        "OC",
+        "MEDN Noise",
+        "MEDN",
+        "MEDN+GODEC",
+        "MEDN+Nuis-Reg",
+        "MEDN+RVT-Reg",
+        "MEDN+RV-Reg",
+        "MEDN+aCompCor",
+        "MEDN+dGSR",
+        "MEDN+MIR",
+        "MEDN+GSR",
+    ]
+    target_file_patterns = {
+        k: op.join(in_dir, "derivatives", v)
+        for k, v in TARGET_FILE_PATTERNS.items()
+        if k in TARGETS
+    }
+
+    run_ddmra_of_rpv(participants_file, target_file_patterns)
+    run_ddmra_of_mean_rv(participants_file, confounds_pattern, target_file_patterns)
+    run_ddmra_of_mean_rvt(participants_file, confounds_pattern, target_file_patterns)
--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
 """Miscellaneous functions used for analyses."""
+import logging
+import os.path as op
+from os import makedirs
+
+import numpy as np
+import pandas as pd
+from ddmra import analysis, plotting, utils
+from nilearn import datasets, input_data
+from scipy.spatial.distance import pdist, squareform
 from scipy.stats import pearsonr
+
+LGR = logging.getLogger("utils")
 
 
 def pearson_r(arr1, arr2, alternative="two-sided"):
@@ -40,6 +51,18 @@ def get_prefixes():
         "dset-cohen": "{participant_id}_task-bilateralfingertapping",
         "dset-dalenberg": "{participant_id}_task-images",
         "dset-dupre": "{participant_id}_task-rest_run-1",
+    }
+    return DATASET_PREFIXES
+
+
+def get_prefixes_mni():
+    """Get the prefixes used for each dataset's functional runs."""
+    DATASET_PREFIXES = {
+        "dset-cambridge": "{participant_id}_task-rest_space-MNI152NLin6Asym",
+        "dset-camcan": "{participant_id}_task-movie_space-MNI152NLin6Asym",
+        "dset-cohen": "{participant_id}_task-bilateralfingertapping_space-MNI152NLin6Asym",
+        "dset-dalenberg": "{participant_id}_task-images_space-MNI152NLin6Asym",
+        "dset-dupre": "{participant_id}_task-rest_run-1_space-MNI152NLin6Asym",
     }
     return DATASET_PREFIXES
 
@@ -87,3 +110,198 @@ def get_target_files():
         ),
     }
     return TARGET_FILES
+
+
+def run_reduced_analyses(
+    files,
+    qc,
+    out_dir=".",
+    confounds=None,
+    n_iters=10000,
+    n_jobs=1,
+    qc_thresh=0.2,
+    window=1000,
+):
+    """Run high-low motion and QCRSFC analyses.
+
+    Parameters
+    ----------
+    files : (N,) list of nifti files
+        List of 4D (X x Y x Z x T) images in MNI space.
+    qc : (N,) array_like
+        List of summary QC values per img (e.g., mean FD).
+    out_dir : str, optional
+        Output directory. Default is current directory.
+    confounds : None or (N,) list of array-like, optional
+        List of 2D (T) numpy arrays with confounds per img.
+        Default is None (no confounds are removed).
+    n_iters : int, optional
+        Number of iterations to run to generate null distributions. Default is 10000.
+    n_jobs : int, optional
+        The number of CPUs to use to do the computation. -1 means 'all CPUs'. Default is 1.
+    window : int, optional
+        Number of units (pairs of ROIs) to include when averaging to generate smoothing curve.
+        Default is 1000.
+
+    Notes
+    -----
+    This function writes out several files to out_dir:
+    - ``analysis_values.tsv.gz``: Raw analysis values for analyses.
+        Has three columns: distance, qcrsfc, and highlow.
+    - ``smoothing_curves.tsv.gz``: Smoothing curve information for analyses.
+        Has three columns: distance, qcrsfc, and highlow.
+    - ``null_smoothing_curves.npz``:
+        Null smoothing curves from each analysis.
+        Contains two 2D arrays, where number of columns is same
+        size and order as distance column in ``smoothing_curves.tsv.gz``
+        and number of rows is number of iterations for permutation analysis.
+        The two arrays' keys are 'qcrsfc' and 'highlow'.
+    - ``[analysis]_analysis.png``: Figure for each analysis.
+    """
+    makedirs(out_dir, exist_ok=True)
+
+    # create LGR with 'spam_application'
+    LGR.setLevel(logging.DEBUG)
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(op.join(out_dir, "log.tsv"))
+    fh.setLevel(logging.DEBUG)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter("%(asctime)s\t%(name)-12s\t%(levelname)-8s\t%(message)s")
+    fh.setFormatter(formatter)
+    # add the handlers to the LGR
+    LGR.addHandler(fh)
+
+    LGR.info("Preallocating matrices")
+    n_subjects = len(files)
+
+    # Load atlas and associated masker
+    atlas = datasets.fetch_coords_power_2011()
+    coords = np.vstack((atlas.rois["x"], atlas.rois["y"], atlas.rois["z"])).T
+    n_rois = coords.shape[0]
+    triu_idx = np.triu_indices(n_rois, k=1)
+    distances = squareform(pdist(coords))
+    distances = distances[triu_idx]
+
+    # Sorting index for distances
+    edge_sorting_idx = distances.argsort()
+    distances = distances[edge_sorting_idx]
+
+    LGR.info("Creating masker")
+    spheres_masker = input_data.NiftiSpheresMasker(
+        seeds=coords,
+        radius=5.0,
+        t_r=None,
+        smoothing_fwhm=4.0,
+        detrend=False,
+        standardize=False,
+        low_pass=None,
+        high_pass=None,
+    )
+
+    # prep for qcrsfc and high-low motion analyses
+    mean_qc = np.array([np.mean(subj_qc) for subj_qc in qc])
+    z_corr_mats = np.zeros((n_subjects, distances.size))
+
+    # Get correlation matrices
+    ts_all = []
+    LGR.info("Building correlation matrices")
+    if confounds:
+        LGR.info("Regressing confounds out of data.")
+
+    for i_subj in range(n_subjects):
+        if confounds:
+            raw_ts = spheres_masker.fit_transform(files[i_subj], confounds=confounds[i_subj]).T
+        else:
+            raw_ts = spheres_masker.fit_transform(files[i_subj]).T
+
+        assert raw_ts.shape[0] == n_rois
+
+        if np.any(np.isnan(raw_ts)):
+            raise ValueError(f"Time series of {files[i_subj]} contains NaNs")
+
+        roi_variances = np.var(raw_ts, axis=1)
+        if any(roi_variances == 0):
+            bad_rois = np.where(roi_variances == 0)[0]
+            raise ValueError(f"ROI(s) {bad_rois} for {files[i_subj]} have variance of 0.")
+
+        ts_all.append(raw_ts)
+        raw_corrs = np.corrcoef(raw_ts)
+        raw_corrs = raw_corrs[triu_idx]
+        raw_corrs = raw_corrs[edge_sorting_idx]  # Sort from close to far ROI pairs
+        z_corr_mats[i_subj, :] = np.arctanh(raw_corrs)
+
+    del (raw_corrs, raw_ts, spheres_masker, atlas, coords)
+
+    analysis_values = pd.DataFrame(columns=["qcrsfc", "highlow"], index=distances)
+    analysis_values.index.name = "distance"
+
+    # QC:RSFC r analysis
+    LGR.info("Performing QC:RSFC analysis")
+    qcrsfc_values = analysis.qcrsfc_analysis(mean_qc, z_corr_mats)
+    analysis_values["qcrsfc"] = qcrsfc_values
+    qcrsfc_smoothing_curve = utils.moving_average(qcrsfc_values, window)
+    qcrsfc_smoothing_curve, smoothing_curve_distances = utils.average_across_distances(
+        qcrsfc_smoothing_curve,
+        distances,
+    )
+
+    # Quick interlude to create the smoothing_curves DataFrame
+    smoothing_curves = pd.DataFrame(
+        columns=["qcrsfc", "highlow"],
+        index=smoothing_curve_distances,
+    )
+    smoothing_curves.index.name = "distance"
+
+    smoothing_curves.loc[smoothing_curve_distances, "qcrsfc"] = qcrsfc_smoothing_curve
+    del qcrsfc_values, qcrsfc_smoothing_curve
+
+    # High-low motion analysis
+    LGR.info("Performing high-low motion analysis")
+    highlow_values = analysis.highlow_analysis(mean_qc, z_corr_mats)
+    analysis_values["highlow"] = highlow_values
+    hl_smoothing_curve = utils.moving_average(highlow_values, window)
+    hl_smoothing_curve, smoothing_curve_distances = utils.average_across_distances(
+        hl_smoothing_curve,
+        distances,
+    )
+    smoothing_curves.loc[smoothing_curve_distances, "highlow"] = hl_smoothing_curve
+    del highlow_values, hl_smoothing_curve
+
+    analysis_values.reset_index(inplace=True)
+    smoothing_curves.reset_index(inplace=True)
+
+    analysis_values.to_csv(
+        op.join(out_dir, "analysis_values.tsv.gz"),
+        sep="\t",
+        line_terminator="\n",
+        index=False,
+    )
+    smoothing_curves.to_csv(
+        op.join(out_dir, "smoothing_curves.tsv.gz"),
+        sep="\t",
+        line_terminator="\n",
+        index=False,
+    )
+
+    # Null distributions
+    LGR.info("Building null distributions with permutations")
+    qcrsfc_null_smoothing_curves, hl_null_smoothing_curves = analysis.other_null_distributions(
+        qc,
+        z_corr_mats,
+        distances,
+        window=window,
+        n_iters=n_iters,
+        n_jobs=n_jobs,
+    )
+
+    np.savez_compressed(
+        op.join(out_dir, "null_smoothing_curves.npz"),
+        qcrsfc=qcrsfc_null_smoothing_curves,
+        highlow=hl_null_smoothing_curves,
+    )
+
+    del qcrsfc_null_smoothing_curves, hl_null_smoothing_curves
+
+    plotting.plot_results(out_dir)
+
+    LGR.info("Workflow completed")
