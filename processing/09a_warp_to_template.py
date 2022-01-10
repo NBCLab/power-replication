6ad8bf32fad4632e581e1a6a3556a4dd091cd8ac
Taylor Salo <tsalo006@fiu.edu>
2022-01-10 18:47:08 -0500

    Try using singularity.

--- processing/09a_warp_to_template.py
+++ processing/09a_warp_to_template.py
@@ -58,7 +58,8 @@ def transform_to_standard_space(
 ):
     print(f"Transforming {native_space_target_file}", flush=True)
     str_ = (
-        f"antsApplyTransforms -i {native_space_target_file} "
+        "singularity exec /home/data/cis/singularity-images/poldracklab-fmriprep_20.2.1.sif "
+        f"antsApplyTransforms -d 4 -e 3 -i {native_space_target_file} "
         f"-r {template} -o {out_file} "
         f"-n LanczosWindowedSinc -t {xform_native_to_t1w} {xform_t1w_to_std}"
     )
