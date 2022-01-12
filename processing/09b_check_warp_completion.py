7a0f58559db739008050f45b6d2ed8e4f695ce1e
Taylor Salo <tsalo006@fiu.edu>
2022-01-12 10:50:34 -0500

    Improve warp check.

--- processing/09b_check_warp_completion.py
+++ processing/09b_check_warp_completion.py
@@ -1,102 +1,64 @@
 """Check that all warped files have been written out."""
 import os.path as op
-from glob import glob
+import sys
 from pprint import pprint
 
+import nibabel as nib
 import pandas as pd
 
-TARGET_FILES = [
-    # TE30
-    "power/{sub}/func/{prefix}_desc-TE30_bold.nii.gz",
-    # OC
-    "tedana/{sub}/func/{prefix}_desc-optcom_bold.nii.gz",
-    # MEDN
-    "tedana/{sub}/func/{prefix}_desc-optcomDenoised_bold.nii.gz",
-    # MEDN+MIR
-    "tedana/{sub}/func/{prefix}_desc-optcomMIRDenoised_bold.nii.gz",
-    # MEDN+MIR Noise
-    "tedana/{sub}/func/{prefix}_desc-optcomMIRDenoised_errorts.nii.gz",
-    # FIT-R2
-    "t2smap/{sub}/func/{prefix}_T2starmap.nii.gz",
-    # FIT-S0
-    "t2smap/{sub}/func/{prefix}_S0map.nii.gz",
-    # MEDN+GODEC (sparse)
-    "godec/{sub}/func/{prefix}_desc-GODEC_rank-4_bold.nii.gz",
-    # MEDN+GODEC Noise (lowrank)
-    "godec/{sub}/func/{prefix}_desc-GODEC_rank-4_lowrankts.nii.gz",
-    # MEDN+dGSR
-    "rapidtide/{sub}/func/{prefix}_desc-lfofilterCleaned_bold.nii.gz",
-    # MEDN+dGSR Noise
-    "rapidtide/{sub}/func/{prefix}_desc-lfofilterCleaned_errorts.nii.gz",
-    # MEDN+aCompCor
-    "nuisance-regressions/{sub}/func/{prefix}_desc-aCompCor_bold.nii.gz",
-    # MEDN+aCompCor Noise
-    "nuisance-regressions/{sub}/func/{prefix}_desc-aCompCor_errorts.nii.gz",
-    # MEDN+GSR
-    "nuisance-regressions/{sub}/func/{prefix}_desc-GSR_bold.nii.gz",
-    # MEDN+GSR Noise
-    "nuisance-regressions/{sub}/func/{prefix}_desc-GSR_errorts.nii.gz",
-    # MEDN+Nuis-Reg
-    "nuisance-regressions/{sub}/func/{prefix}_desc-NuisReg_bold.nii.gz",
-    # MEDN Noise
-    "tedana/{sub}/func/{prefix}_desc-optcomDenoised_errorts.nii.gz",
-]
-PHYSIO_TARGET_FILES = [
-    # MEDN+RV-Reg
-    "nuisance-regressions/{sub}/func/{prefix}_desc-RVReg_bold.nii.gz",
-    # MEDN+RVT-Reg
-    "nuisance-regressions/{sub}/func/{prefix}_desc-RVTReg_bold.nii.gz",
-]
+sys.path.append("..")
+
+from utils import get_prefixes, get_prefixes_mni, get_target_files  # noqa: E402
 
 if __name__ == "__main__":
     PROJECT_DIR = "/home/data/nbc/misc-projects/Salo_PowerReplication/"
     participants_file = op.join(PROJECT_DIR, "participants.tsv")
+
+    target_files = get_target_files()
+    dset_prefixes = get_prefixes()
+    dset_mni_prefixes = get_prefixes_mni()
+
     bad_subs = []
     df = pd.read_table(participants_file)
     for _, row in df.iterrows():
         dataset = row["dset"]
         sub = row["participant_id"]
+        dset_mni_subj_prefix = dset_mni_prefixes[dataset].format(participant_id=sub)
+        dset_subj_prefix = dset_prefixes[dataset].format(participant_id=sub)
+
         deriv_dir = op.join(PROJECT_DIR, dataset, "derivatives")
 
-        medn_files = sorted(
-            glob(
-                op.join(
-                    deriv_dir,
-                    "tedana",
-                    sub,
-                    "func",
-                    "*_space-MNI152NLin6Asym_desc-optcomDenoised_bold.nii.gz",
-                )
-            )
+        medn_mni_file = op.join(
+            deriv_dir,
+            "tedana",
+            sub,
+            "func",
+            target_files["MEDN"].format(
+                prefix=dset_mni_subj_prefix,
+                participant_id=sub,
+            ),
         )
-        if len(medn_files) != 1:
+        if not op.isfile(medn_mni_file):
             print(f"Dataset {dataset} subject {sub} missing MEDN file.")
             bad_subs.append((dataset, sub))
             continue
 
-        medn_file = medn_files[0]
-        medn_name = op.basename(medn_file)
-        prefix = medn_name.split("desc-")[0].rstrip("_")
+        for filetype, target_file in target_files.items():
+            if ("MEDN+RV" in filetype) and (dataset != "dset-dupre"):
+                # Skip physio derivatives for datasets without physio data
+                continue
 
-        for target_file in TARGET_FILES:
-            full_file_pattern = op.join(
-                deriv_dir, target_file.format(sub=sub, prefix=prefix)
+            full_target_file = op.join(
+                deriv_dir,
+                target_file.format(participant_id=sub, prefix=dset_mni_subj_prefix),
             )
-            matching_files = sorted(glob(full_file_pattern))
-            if len(matching_files) == 0:
-                print(f"Dataset {dataset} subject {sub} missing {full_file_pattern}")
+            if not op.isfile(full_target_file):
+                print(f"Dataset {dataset} subject {sub} missing {full_target_file}")
                 bad_subs.append((dataset, sub))
-
-        if dataset == "dset-dupre":
-            for target_file in PHYSIO_TARGET_FILES:
-                full_file_pattern = op.join(
-                    deriv_dir, target_file.format(sub=sub, prefix=prefix)
-                )
-                matching_files = sorted(glob(full_file_pattern))
-                if len(matching_files) == 0:
-                    print(
-                        f"Dataset {dataset} subject {sub} missing {full_file_pattern}"
-                    )
+            else:
+                img = nib.load(full_target_file)
+                if len(img.shape) != 4:
+                    print(f"Dataset {dataset} subject {sub} failed {full_target_file}")
                     bad_subs.append((dataset, sub))
 
     bad_subs = sorted(list(set(bad_subs)))
