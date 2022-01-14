db39fd7b4d6da70606ae61379f43f7a032414f1b
Taylor Salo <tsalo006@fiu.edu>
2022-01-14 17:33:36 -0500

    Update 06_ica_global_signal.py

--- experiment-1/06_ica_global_signal.py
+++ experiment-1/06_ica_global_signal.py
@@ -226,7 +226,6 @@ def correlate_medn_with_oc(
     # Convert r values to normally distributed z values with Fisher's
     # transformation (not test statistics though)
     r_values = out_df["correlation"].values.astype(float)
-    print(r_values)
     z_values = np.arctanh(r_values)
     mean_z = np.mean(z_values)
     sd_z = np.std(z_values)
